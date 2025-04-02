79c6f61b93591c43ffef147695aa26a8f09c64cc
sudeep-devkota <sudeepdevkota639@gmail.com>
2025-04-01 23:41:31 -0400

    all done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import Hero from '../components/hero'
+import Hero from '../components/Hero'
 import Blogs from '../components/Blogs'
 import Footer from '../components/Footer'
 
