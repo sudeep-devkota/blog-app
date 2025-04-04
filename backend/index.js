5489d24e86c7b829998e102e9a17b91604aa1df1
sudeep-devkota <sudeepdevkota639@gmail.com>
2025-04-04 09:31:18 -0400

    core

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({extended:true}));
     
 app.use(cors({
     origin: [
-      'http://localhost:5173',
+      'http://localhost:5000',
       'https://blog-app-kl9i.vercel.app'
     ],
     credentials: true
